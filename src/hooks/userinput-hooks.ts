52e6b7bb93db1ac95b02e4251662e896b1730745
luck_nut <hasyrawi@gmail.com>
2020-09-30 20:39:42 +0800

    user input handle onChange

---
+++ src/hooks/userinput-hooks.ts
@@ -0,0 +1,11 @@
+import { useCallback, useState } from "react";
+
+const useUserInput = (defaultValue: string = '') => {
+  const [value, setValue] = useState(defaultValue);
+
+  const onChange = useCallback((e) => setValue(e.target.value), [])
+
+  return { value, onChange }
+}
+
+export default useUserInput;
