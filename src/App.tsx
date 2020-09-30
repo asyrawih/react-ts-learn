ad3cbaf3c9646d1531733e8db6e1f676db7f07ca
luck_nut <hasyrawi@gmail.com>
2020-09-30 20:39:50 +0800

    implements the hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { TextField } from './components/TextField/TextField';
-import { Form } from './shared/Form';
+import useBahasa from './hooks/language-hooks';
+import useSearchable from './hooks/searchable-hooks';
+import useUserInput from './hooks/userinput-hooks';
 
-function App() {
+const App: React.FC = () => {
+
+  const bahasa = useBahasa()
+
+  const userInput = useUserInput('')
+
+  const search = useSearchable(
+    bahasa,
+    userInput.value,
+    (item) => [item.name]
+  )
+
+  console.log(bahasa);
   return (
     <div className="App">
-     <Form>
-      <TextField /> 
-     </Form>
+      <input type="text" {...userInput} />
+
+      <ul>
+        {
+          search.map(item => (
+            <li>{item.name}</li>
+          ))
+        }
+      </ul>
+
     </div>
   );
 }
